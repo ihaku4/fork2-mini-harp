8b9a3726d62a0243a33452f938041e0d4324a4dd
Durrrr <ihaku4@gmail.com>
2014-04-02 17:13:21 +0800

    less-preprocess

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
 var connect = require('connect');
 var serveStatic = require("serve-static");
 var fs = require("fs");
-var jadePath = "verify/assets/foo.jade"
 var assetsPath = "verify/assets"
 var jade = require("jade");
+var less = require("less");
 
 var app = function(root) {
     return connect()
 //        .use(serveStatic(root))
         .use( function(req, res, next) {
-            console.log(req.url);
             if (req.url === "/foo.html") {
-                debugger;
-                fs.readFile(jadePath, function(err, data) {
+                fs.readFile(assetsPath + "/foo.jade", function(err, data) {
                    if (err) throw err;
                    jade.render(data, function(err, html) {
                         if (err) next();
                         res.end(html);
                    });
                 });
-            } else if (req.url === "/not-found.html") {
-                res.statusCode = 404;
-                res.end();
             } else if (req.url === "/bar.html") {
 //                serveStatic(assetsPath + req.url)(req, res, next);
-//                debugger;
                 fs.readFile(assetsPath + req.url, function(err, data) {
                     if (err) throw err;
                     res.end(data);
-                    console.log(data);
+                });
+            } else if (req.url === "/foo.css") {
+                fs.readFile(assetsPath + "/foo.less", function(err, data) {
+                    if (err) throw err;
+                    data = new String(data);
+                    less.render(data, function(err, css) {
+                        if (err) next();
+                        res.end(css);
+                    });
+                })
+            } else if (req.url === "/bar.css") {
+//                serveStatic(assetsPath + req.url)(req, res, next);
+                fs.readFile(assetsPath + req.url, function(err, data) {
+                    if (err) throw err;
+                    res.end(data);
                 });
             } else next();
         } );
