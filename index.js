c163c39a03cd842ccfa737250e0a3e97ce55a49b
Durrrr <ihaku4@gmail.com>
2014-04-02 12:11:32 +0800

    jade-preprocess

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 var connect = require('connect');
 var serveStatic = require("serve-static");
+var fs = require("fs");
+var jadePath = "verify/assets/foo.jade"
+var assetsPath = "verify/assets"
+var jade = require("jade");
 
 var app = function(root) {
     return connect()
-        .use(serveStatic(root, {index: "index.js"}));
+//        .use(serveStatic(root))
+        .use( function(req, res, next) {
+            console.log(req.url);
+            if (req.url === "/foo.html") {
+                debugger;
+                fs.readFile(jadePath, function(err, data) {
+                   if (err) throw err;
+                   jade.render(data, function(err, html) {
+                        if (err) next();
+                        res.end(html);
+                   });
+                });
+            } else if (req.url === "/not-found.html") {
+                res.statusCode = 404;
+                res.end();
+            } else if (req.url === "/bar.html") {
+//                serveStatic(assetsPath + req.url)(req, res, next);
+//                debugger;
+                fs.readFile(assetsPath + req.url, function(err, data) {
+                    if (err) throw err;
+                    res.end(data);
+                    console.log(data);
+                });
+            } else next();
+        } );
 }
 module.exports = app;
